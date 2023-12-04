3ed337d5a9a8206e16af841254d1619eee90254b
Reinhardt Lagat <56647015+Reinhardt254@users.noreply.github.com>
2023-12-04 12:35:43 +0300

    changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -13,5 +13,20 @@ export class AuthController {
   @Post('signin')
   signin() {
     return this.authService.signin();
+  }
+
+  @Get('users')
+  getusers() {
+    return this.authService.signin();
+  }
+
+  @Put('updated')
+  updateuser() {
+    return this.authService.signup();
+  }
+
+  @Delete('users')
+  deleteuser() {
+    return this.authService.signup();
   }
 }
